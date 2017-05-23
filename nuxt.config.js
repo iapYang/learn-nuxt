fcca60d0aaa702f9b6c5032e8cab1f34e1dc4d4c
iapYang <yklorse@gmail.com>
2017-05-23 15:47:03 +0800

    success

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    head: {
+        meta: [{
+            charset: 'utf-8',
+        }, {
+            name: 'viewport', content: 'width=device-width, initial-scale=1',
+        }, {
+            hid: 'description', name: 'description', content: 'Nuxt.js project',
+        }],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+        ],
+    },
+    srcDir: 'src/',
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      sjffjsguwufgusgjgsugusuhusghuhguh
+      <img src="../assets/640x480_Mobile.jpg" alt="">      
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss">
+
+</style>
+
